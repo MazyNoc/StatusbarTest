adff45bb73f20702c28c3e6ebbe7ef29a31f92af
MazyNoc <MazyNoc@gmail.com>
2016-03-19 08:23:43 +0100

    added rescale flag and updated the main view

--- app/src/main/java/nu/annat/statusbartest/MainActivity.java
+++ app/src/main/java/nu/annat/statusbartest/MainActivity.java
@@ -1,13 +1,19 @@
 package nu.annat.statusbartest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final boolean SHOW_UNDER_STATUS_BUT_RESCALE_FAILS = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (SHOW_UNDER_STATUS_BUT_RESCALE_FAILS) {
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+		}
 		setContentView(R.layout.activity_main);
 	}
 }
